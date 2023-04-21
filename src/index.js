5e5ac63c5fc84daffbd507a048781c13298c0158
Gr1sh-19 <59308408+Gr1sh-19@users.noreply.github.com>
2023-04-21 08:08:21 +0200

    current date function and added search element

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 const apiKey = "d30352adf546160cc3b7co4bf1t16aee";
 
 function showCityName(response) {
+  let dateElement = document.querySelector("#currrent-date");
+  let date = new Date();
+  month = date.getMonth();
+  let months = ["January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"
+  ];
+  month = months[month];
+  console.log(month);
+  day = date.getDay();
+  days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  day = days[day];
+  console.log(day);
+  currentDate = date.getDate();
+  console.log(currentDate);
+  let today = `${day}, ${month} ${currentDate}`;
+  dateElement.innerHTML = today;
+
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
   let conditionsElement = document.querySelector("#weather-conditions");
