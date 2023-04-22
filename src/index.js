fe0c98a671021c31dc0962e51109854ffe6ca08a
Gr1sh-19 <59308408+Gr1sh-19@users.noreply.github.com>
2023-04-22 10:51:56 +0200

    removed redundant code

--- src/index.js
+++ src/index.js
@@ -1,41 +1,5 @@
 const apiKey = "d30352adf546160cc3b7co4bf1t16aee";
 
-function showDefaultCity(response) {
-  let dateElement = document.querySelector("#currrent-date");
-  let date = new Date();
-  month = date.getMonth();
-  let months = ["January", "February", "March", "April", "May", "June", "July",
-    "August", "September", "October", "November", "December"
-  ];
-  month = months[month];
-  day = date.getDay();
-  days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  day = days[day];
-  currentDate = date.getDate();
-  let today = `${day}, ${month} ${currentDate}`;
-  dateElement.innerHTML = today;
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.city;
-  let conditionsElement = document.querySelector("#weather-conditions");
-  conditions = response.data.condition.description;
-  conditionsElement.innerHTML = conditions.charAt(0).toUpperCase() + conditions.slice(1);
-  let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = response.data.temperature.humidity;
-  let windElement = document.querySelector("#wind");
-  windElement.innerHTML = response.data.wind.speed;
-  let currentTemperatureElement = document.querySelector("#current-temperature");
-  currentTemperatureElement.innerHTML = Math.round(response.data.temperature.current);
-  let imageElement = document.querySelector("#current-temperature-img");
-  imageElement.setAttribute("src", response.data.condition.icon_url);
-}
-
-let cityElement = document.querySelector("#city");
-cityName = "Munich";
-let cityUrl = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${apiKey}&units=metric`;
-axios.get(cityUrl).then(showDefaultCity);
-
-/* search a city */
-
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let month = date.getMonth();
@@ -49,6 +13,8 @@ function formatDate(timestamp) {
   currentDate = date.getDate();
   return `${day}, ${month} ${currentDate}`;
 }
+
+/* search a city */
 
 function updateValues(response) {
 
@@ -87,3 +53,11 @@ function searchCity(event) {
 
 let searchElement = document.querySelector("#search-input");
 searchElement.addEventListener("submit", searchCity);
+
+
+/* default city */
+
+let cityElement = document.querySelector("#city");
+cityName = "Munich";
+let cityUrl = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${apiKey}&units=metric`;
+axios.get(cityUrl).then(updateValues);
